2b9c32308d992562f174457b3da655e3577062c8
cmaster11 <alberto.marchetti@smartly.io>
2022-10-03 11:35:45 +0300

    wip promote

--- auto-tagger/scripts/create_branch.sh
+++ auto-tagger/scripts/create_branch.sh
@@ -3,6 +3,7 @@ set -Eeumo pipefail
 
 tag_prefix="$TAG_PREFIX"
 latest_tag="$LATEST_TAG"
+force_push="$FORCE_PUSH"
 tag_without_prefix=$(echo "$latest_tag" | sed -E "s/^${tag_prefix}v//")
 
 if [[ "$tag_without_prefix" =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
@@ -10,10 +11,19 @@ if [[ "$tag_without_prefix" =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
 
   branch_name="${tag_prefix}v${major}"
 
+  if [[ "$force_push" == "true" ]]; then
+    git branch -d "$branch_name" || true
+  fi
+
   # Create and push branch
   git checkout -b "$branch_name"
   git branch --set-upstream-to="origin/$branch_name" || true
-  git push -u origin "$branch_name"
+
+  if [[ "$force_push" == "true" ]]; then
+    git push --force -u origin "$branch_name"
+  else
+    git push -u origin "$branch_name"
+  fi
 
 else
   echo "Bad tag format";
