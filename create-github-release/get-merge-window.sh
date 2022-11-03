df885089b8b9e028598b18c581818e45e7e47d5f
Simon Jagoe <simon.jagoe@smartly.io>
2022-11-03 10:43:10 +0200

    Fix date format, dounle % is not needed when not in a kojo template file

--- create-github-release/get-merge-window.sh
+++ create-github-release/get-merge-window.sh
@@ -7,8 +7,8 @@ this_tag="${TAG_NAME/#refs\/tags\//}"
 recent_tags="$(git tag --sort=creatordate --list 'v*' | grep -P 'v\d+\.\d+\.\d+' | grep --color=no -B1 "^$this_tag\$")"
 previous_tag="$(echo "$recent_tags" | head -n1)"
 
-previous_tag_date="$(git log -1 --tags --no-walk --pretty="format:%%cI" "$previous_tag")"
-this_tag_date="$(git log -1 --tags --no-walk --pretty="format:%%cI" "$this_tag")"
+previous_tag_date="$(git log -1 --tags --no-walk --pretty="format:%cI" "$previous_tag")"
+this_tag_date="$(git log -1 --tags --no-walk --pretty="format:%cI" "$this_tag")"
 
 start_date="$(date -Is -d "$previous_tag_date")"
 end_date="$(date -Is -d "$this_tag_date")"
