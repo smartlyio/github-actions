9174554f299976ad593df13972638c0921dd00fd
Simon Jagoe <simon.jagoe@smartly.io>
2022-11-03 13:21:35 +0200

    Support different tag filters for formats other than vN.N.N

--- create-github-release/get-merge-window.sh
+++ create-github-release/get-merge-window.sh
@@ -2,9 +2,16 @@
 
 set -euo pipefail
 
+tag_search="${TAG_SEARCH_PREFIX}*"
 this_tag="${TAG_NAME/#refs\/tags\//}"
 
-recent_tags="$(git tag --sort=creatordate --list 'v*' | grep -P 'v\d+\.\d+\.\d+' | grep --color=no -B1 "^$this_tag\$")"
+tags_list="$(git tag --sort=creatordate --list "$tag_search")"
+if [[ "$TAG_SEARCH_REGEX" == "" ]]; then
+    filtered_tags_list="$tags_list"
+else
+    filtered_tags_list="$(echo "$tags_list" | grep -P "$TAG_SEARCH_REGEX")"
+fi
+recent_tags="$(echo "$filtered_tags_list" | grep --color=no -B1 "^$this_tag\$")"
 previous_tag="$(echo "$recent_tags" | head -n1)"
 
 previous_tag_date="$(git log -1 --no-walk --pretty="format:%cI" "$previous_tag")"
@@ -12,6 +19,9 @@ this_tag_date="$(git log -1 --no-walk --pretty="format:%cI" "$this_tag")"
 
 start_date="$(date -Is -d "$previous_tag_date")"
 end_date="$(date -Is -d "$this_tag_date")"
+
+echo "Previous tag: $previous_tag at $start_date"
+echo "This tag: $this_tag at $end_date"
 
 {
   echo "start_date=$start_date"
